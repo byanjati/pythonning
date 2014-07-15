a4dc8ecd4b5e9ba936b7a5b9734105f8a4f919fd
Byan <byanjati@gmail.com>
2014-07-15 13:25:54 +0800

    flower , double clover 10
    coincidental event

--- flower.py
+++ flower.py
@@ -8,20 +8,20 @@ def bob_line(bob,length,n):
     lt(bob,n)
 
 def first_try(f,bob,length,n):
-    for i in range(45):
+    for i in range(65):
         f(bob,length,n)
 
 def second_try(f,bob,length,n):
-    lt(bob,135)
-    for i in range(45):
+    lt(bob,115)
+    for i in range(65):
         f(bob,length,n)
         
 def one_clover():
-    first_try(bob_line,bob,4,1)
-    second_try(bob_line,bob,4,1)
+    first_try(bob_line,bob,2,1)
+    second_try(bob_line,bob,2,1)
     lt(bob,180)
 
-for i in range(8):
+for i in range(11):
     one_clover()
 
 wait_for_user()
